e9a525c013f88e1d05d626449225bf7b13ca4890
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2025-12-30 13:13:39 +0100

    fix: make it cleaner

--- azure_upload.ps1
+++ azure_upload.ps1
@@ -1,23 +1,25 @@
 # Load config
 $config = Get-Content -Raw -Path ".\azure_config.json" | ConvertFrom-Json
 
-# dotnet build --configuration Release
+# build
+Write-Output "build..."
+dotnet build --configuration Release
 
 # cleanup
-$source = "C:\Users\dobin\source\repos\DetonatorAgent\bin\Release\net8.0\*"
+$source = "bin\Release\net8.0\*"
 $destination = "$env:TEMP\detonatoragent.zip"
 
 if (Test-Path $destination) {
-  Write-Output "cleanup..."
+  Write-Output "cleanup: $destination"
   Get-ChildItem $destination | Remove-Item -Force -ErrorAction SilentlyContinue
 }
 
 # make a zip
-Write-Output "zip..."
+Write-Output "zip: $source to $destination"
 Compress-Archive -Path $source -DestinationPath $destination -Force
 
 # Upload zip as blob
-Write-Output "upload..."
+Write-Output "upload: $destination"
 az storage blob upload `
   --account-name $($config.StorageAccount) `
   --container-name $($config.ContainerName) `
